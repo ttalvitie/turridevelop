c94b2c18b7a39a006e9197556a4612d0a3e47b9b
Topi Talvitie <topi.talvitie@iki.fi>
2012-12-27 17:24:29 +0200

    Siirretty JSON oikeaan paikkaan, lisätty file-paketti.

---
+++ projekti/src/main/java/fi/helsinki/cs/turridevelop/file/TurrOutput.java
@@ -0,0 +1,13 @@
+package fi.helsinki.cs.turridevelop.file;
+
+import fi.helsinki.cs.turridevelop.logic.Machine;
+import org.json.JSONObject;
+
+/**
+ * Functions for writing Turr files.
+ */
+public class TurrOutput {
+    public static JSONObject machineToJSON(Machine machine) {
+        return null;
+    }
+}
